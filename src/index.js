1bf9b0e39555cd9dedbfb2df830ff0d535e53761
San <San@SanF-MacBook-Air.local>
2022-11-07 13:09:03 +0000

    cbf tdd fizz buzz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 export const projectName = () => {
   return 'Kata starter project';
 };
+
+export const fizzbuzz = (number) => {
+  return 1;
+};
+
+export const fizz = (number) => {
+  return "fizz";
+};
+
+export const buzz = (number) => {
+  return "buzz";
+};
+
+export const fizzybuzz = (number) => {
+  return "fizzybuzz";
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,31 @@
-import {projectName} from "./index";
+import {fizzybuzz, buzz, fizz, fizzbuzz, projectName} from "./index";
 
 describe('Starter project should', () => {
   it('output its name', () => {
     expect(projectName()).toEqual('Kata starter project');
+  });
+});
+
+describe('Fizzbuzz function should', () => {
+  it('output 1 when given 1', () => {
+    expect(fizzbuzz(1)).toEqual(1);
+  });
+});
+
+describe('Fizz function should', () => {
+  it('output fizz when given 3', () => {
+    expect(fizz(3)).toEqual("fizz");
+  });
+});
+
+describe('Buzz function should', () => {
+  it('output buzz when given 5', () => {
+    expect(buzz(5)).toEqual("buzz");
+  });
+});
+
+describe('fizzybuzz function should', () => {
+  it('output fizzybuzz when given 15', () => {
+    expect(fizzybuzz(15)).toEqual("fizzybuzz");
   });
 });
